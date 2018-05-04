6e55525f6128770bf78c982a4cd12e0c7ba8018e
Matthew Smith <azamatsmith@gmail.com>
2018-05-04 06:30:32 -0600

    Update <Map /> - add code to check for bounds changed

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -1,32 +1,50 @@
-import React from "react";
-import { compose, withProps } from "recompose";
+import React from 'react';
+import {compose, withHandlers, withProps} from 'recompose';
 import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  Marker
-} from "react-google-maps";
+  Marker,
+} from 'react-google-maps';
 
 const Map = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyB-wDNI3bGVreubWNQkWvjHlL15a7Bcx48&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyB-wDNI3bGVreubWNQkWvjHlL15a7Bcx48&v=3.exp&libraries=geometry,drawing,places',
+    loadingElement: <div style={{height: `100%`}} />,
+    containerElement: <div style={{height: `400px`}} />,
+    mapElement: <div style={{height: `100%`}} />,
+  }),
+  withHandlers(() => {
+    const refs = {
+      map: undefined,
+    };
+
+    return {
+      onMapMounted: () => ref => {
+        console.log('had ref now');
+        refs.map = ref;
+      },
+      onZoomChanged: ({onZoomChange}) => () => {
+        console.log('zoomChanged', refs.map.getZoom());
+      },
+      onBoundsChanged: obj => () => {
+        console.log('boundsChanged', obj);
+      },
+    };
   }),
   withScriptjs,
   withGoogleMap
 )((props, context) => (
   <GoogleMap
     defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
-    onCenterChanged={() => console.log(context)}
-    getBounds={props.getBounds}
+    defaultCenter={{lat: -34.397, lng: 150.644}}
+    ref={props.onMapMounted}
+    onBoundsChanged={props.onBoundsChanged}
   >
     {props.isMarkerShown && (
       <Marker
-        position={{ lat: -34.397, lng: 150.644 }}
+        position={{lat: -34.397, lng: 150.644}}
         onClick={props.onMarkerClick}
       />
     )}
@@ -35,7 +53,7 @@ const Map = compose(
 
 class MyFancyComponent extends React.PureComponent {
   state = {
-    isMarkerShown: false
+    isMarkerShown: false,
   };
 
   componentDidMount() {
@@ -44,12 +62,12 @@ class MyFancyComponent extends React.PureComponent {
 
   delayedShowMarker = () => {
     setTimeout(() => {
-      this.setState({ isMarkerShown: true });
+      this.setState({isMarkerShown: true});
     }, 3000);
   };
 
   handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false });
+    this.setState({isMarkerShown: false});
     this.delayedShowMarker();
   };
 
